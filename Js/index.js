f945d66eef1f31840ae729a9ec62b55072f3082a
joseph oldfield <joseph.t.oldfield@gmail.com>
2019-10-31 09:54:32 +0000

    score system added - This willl be changed to a wave system

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,12 @@
 
 //Current hero position tracker
 let hero = {
-    top: 700,
-    left: 550
+    top: 600,
+    left: 550,
 }
 
 //Current missile position tracker
-let missiles = [
-]
+let missiles = []
 let enemies = [
 {left: 200, top: 100},
 {left: 300, top: 100},
@@ -25,6 +24,15 @@ let enemies = [
 {left: 700, top: 175},
 {left: 800, top: 175},
 {left: 900, top: 175},
+{left: 200, top: 250},
+{left: 300, top: 250},
+{left: 400, top: 250},
+{left: 500, top: 250},
+{left: 600, top: 250},
+{left: 700, top: 250},
+{left: 800, top: 250},
+{left: 900, top: 250},
+
 
 ]
 
@@ -37,14 +45,14 @@ document.onkeydown = function (e) {
     //Move left
     if (e.keyCode === 37) {
         console.log("left")
-        hero.left = hero.left - 10
+        hero.left = hero.left - 15
         moveHero()
     }
     //Move Right
     else if (e.keyCode === 39) {
 
         console.log("right")
-        hero.left = hero.left + 10
+        hero.left = hero.left + 15
         moveHero()
     }
     //Fire Missile by adding to array
@@ -55,6 +63,11 @@ document.onkeydown = function (e) {
             top: hero.top
         })
     }
+}
+
+//Function to update score 
+function playerScore() {
+    document.getElementById('score').innerHTML = `${Math.round(1700 / (enemies.length +1)) -68}`
 }
 // function to track player movement
 function moveHero() {
@@ -70,13 +83,6 @@ function drawMissiles() {
                 top:${missiles[missile].top}px;'></div>`;
     }
 }
-function moveMissiles() {
-    for (let missile = 0; missile < missiles.length; missile = missile + 1) {
-        missiles[missile].top = missiles[missile].top - 5;
-    }
-}
-
-
 // Function for missile movement 
 function moveMissiles() {
     for (let missile = 0; missile < missiles.length; missile = missile + 1) {
@@ -117,18 +123,22 @@ function collisionDetect() {
     }
 }
 
+
+
+
 // This Gameloop function runs the game by repeat executing track functions
 function gameloop() {
-    setTimeout(gameloop, 50)
+    setTimeout(gameloop, 50);
     moveMissiles();
     drawMissiles();
     moveEnemies();
     drawEnemies();
     collisionDetect();
+    playerScore();
 }
 gameloop()
 
-//gamne loop keeps game working
+
 
 
 
